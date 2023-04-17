41b4c054d7b8649ea4f9165b0c4b46278297e410
Ligma Balls <d.ramos100402@gmail.com>
2023-04-17 09:14:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './Data';
+import Student from './components/Student';
 
 function App() {
+
+  const [studentData, setStudentData] = useState({ students: data });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student Scores</h1>
+      {studentData.students.map((student, index) => <Student student={student} key={index} />)}
     </div>
   );
 }
